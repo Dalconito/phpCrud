e98a619513bac47a3de0e45d5b1aee1238804f0b
Leoni Terto <terto.leoni@gmail.com>
2024-07-25 20:10:51 -0300

    Adicionada pasta de css

--- Controllers/createProduto.php
+++ Controllers/createProduto.php
@@ -1,48 +1,53 @@
 <?php
-    require 'Produtos.php';
+require 'Produtos.php';
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Adicionar Produtos</title>
-    <link href="../uicons-regular-rounded/webfonts/uicons-regular-rounded.css" rel="stylesheet"> <!--load all styles -->
-    <link rel="stylesheet" href="../public/createProduto.css">
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Adicionar Produtos</title>
+  <link href="../uicons-regular-rounded/webfonts/uicons-regular-rounded.css" rel="stylesheet"> <!--load all styles -->
+  <link rel="stylesheet" href="../public/createProduto.css">
 </head>
 <body>
-    <a class="voltar" href="../index.php"><i class="fi fi-rr-arrow-to-left icone"></i>Voltar</a>
-    <div class="principalDiv">
-
+  <a class="voltar" href="../index.php"><i class="fi fi-rr-arrow-to-left icone"></i>Voltar</a>
+  <div class="bodyDiv">
+  <div class="principalDiv">
     <form method="post">
-    <table class="tabelaResponsiva">
-        <tr>
-          <th>Código</th>
-          <th>Nome</th>
-          <th>Descrição</th>
-          <th>Quantidade</th>
-          <th>Ações</th>
-        </tr>
-
-          <tr>
-            <td data-label="" ><input type="number" id="codProduto" name="codProduto" required></td>
-            <td data-label="" ><input type="text" id="nomeProduto" name="nomeProduto" required></td>
-            <td data-label="" ><input type="text" id="descProduto" name="descProduto" required></td>
-            <td data-label="" ><input type="text" id="qtdeProduto" name="qtdeProduto" required></td>
-            <td data-label="" ><input type="submit" value="Validar"></td>
-          </tr>
-    </table>
-   </form>
-    <?php
-        $postProduto = $_POST;
-        $getProduto = isset($_GET['id']) ? $_GET['id'] : null;
-        $codProduto = isset($_POST['codProduto']) ? $_POST['codProduto'] : null;
-        $nomeProduto = isset($_POST['nomeProduto']) ? $_POST['nomeProduto'] : null;
-        $descProduto = isset($_POST['descProduto']) ? $_POST['descProduto'] : null;
-        $qtdeProduto = isset($_POST['qtdeProduto']) ? $_POST['qtdeProduto'] : null;
-        isset($_POST['codProduto']) ? createProduto($codProduto, $nomeProduto, $descProduto, $qtdeProduto): null;
-        ?>
+      <div class="tabelaResponsiva">
+        <div class="campoTabela">
+          <label class="th">Código</label><br>
+          <input class="td" type="number" id="codProduto" name="codProduto" required></td>
         </div>
+        <div class="campoTabela">
+          <label class="th">Nome</label><br>
+          <input class="td" type="text" id="nomeProduto" name="nomeProduto" required></td>
+        </div>
+        <div class="campoTabela">
+          <label class="th">Descrição</label><br>
+          <input class="td" type="text" id="descProduto" name="descProduto" required></td>
+        </div>
+        <div class="campoTabela">
+          <label class="th">Quantidade</label><br>
+          <input class="td" type="text" id="qtdeProduto" name="qtdeProduto" required></td>
+        </div>
+        <div class="campoTabela">
+          <input class="td" type="submit" value="Validar"></td>
+        </div>
+      </div>
+    </form>
+    <?php
+    $postProduto = $_POST;
+    $getProduto = isset($_GET['id']) ? $_GET['id'] : null;
+    $codProduto = isset($_POST['codProduto']) ? $_POST['codProduto'] : null;
+    $nomeProduto = isset($_POST['nomeProduto']) ? $_POST['nomeProduto'] : null;
+    $descProduto = isset($_POST['descProduto']) ? $_POST['descProduto'] : null;
+    $qtdeProduto = isset($_POST['qtdeProduto']) ? $_POST['qtdeProduto'] : null;
+    isset($_POST['codProduto']) ? createProduto($codProduto, $nomeProduto, $descProduto, $qtdeProduto): null;
+    ?>
+  </div>
+  </div>
 </body>
 </html>
--- Controllers/deleteProduto.php
+++ Controllers/deleteProduto.php
@@ -4,16 +4,7 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Deletar Produtos</title>
-        <style>
-            .btn{height: 100px; width: 100px; background-color: brown; color: white;}
-            body{margin: 0 auto; display: grid; align-items: center; justify-content: center; background-color: #b4b3b3;}
-            .btnSimDiv{ padding-top: 100px;margin: 0 auto; display: flex; gap: 30px; justify-content: center;}
-            table{border-collapse:separate; border-spacing: 10px; 
-            border: 4px solid black; margin: 0 auto;}
-      table th, table td{padding: 10px; border: 2px solid black}
-      table th{background-color: #e6daa8; text-align: left;}
-      table tr:nth-child(even){background-color: #f9f9f9;}
-        </style>
+        <link rel="stylesheet" href="../public/deleteProduto.css">
     </head>
     <?php
         require './Produtos.php';
@@ -40,6 +31,28 @@
             <td><?php echo $produtos['qtdeProduto'] ?></td>
         </tr>
         </table>
+
+    <h1>Tem certeza que Deseja excluir? O item: </h1>
+    <div class="principalDiv">
+        <div class="bodyDiv">
+            <div class="campoTabela">
+                <label for="" class="th"> Código do Produto</th></label><br>
+                <label for="" class="td"><?php echo $getcod ?></td></label>
+            </div>
+            <div class="campoTabela">
+                <label for="" class="td"><?php echo $produtos['nomeProduto'] ?></td></label><br>
+                <label for="" class="th"> Nome do produto</th></label>
+            </div>
+            <div class="campoTabela">
+                <label for="" class="td"><?php echo $produtos['descProduto'] ?></td></label><br>
+                <label for="" class="th"> Descrição do Produto do produto</th></label>
+            </div>
+            <div class="campoTabela">
+                <label for="" class="th"> Quantidade</th></label><br>
+                <label for="" class="td"><?php echo $produtos['qtdeProduto'] ?></td></label>
+            </div>
+        </div>
+    </div>
         <div class="btnSimDiv"><form method="post"><input type="submit" value="Sim" name="resposta" class="btn"></form>
         <div class="btnNaoDiv"><form action="../index.php" method="post"><input type="submit" value="Nao" name="resposta" class="btn"></form></div>
     </div>
