d5d3c1ebc4bd736a75385bf6ec659ea7a10bf23f
Leoni Terto <terto.leoni@gmail.com>
2024-07-31 15:28:32 -0300

    Adicionada pasta de css

--- Controllers/updateProduto.php
+++ Controllers/updateProduto.php
@@ -15,7 +15,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Adicionar Produtos</title>
+    <title>Update CRUD</title>
     <link href="../uicons-regular-rounded/webfonts/uicons-regular-rounded.css" rel="stylesheet"> <!--load all styles -->
     <link rel="stylesheet" href="../public/updateProduto.css">
     
--- index.php
+++ index.php
@@ -5,10 +5,34 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="./uicons-regular-rounded/webfonts/uicons-regular-rounded.css" rel="stylesheet"> <!--load all styles -->
     <link rel="stylesheet" href="./public/index.css">
-    <title>Pagina Inicial</title>
+    <title>Sistema CRUD</title>
   </head>
 
-<?php require './controllers/Produtos.php'; $resultQuery = getProduto(); ?>
+<?php require './controllers/Produtos.php'; $resultQuery = getProduto();
+
+// Nome do arquivo de log
+$logFile = 'access_log.txt';
+
+// Capturar o endereço IP do visitante
+$ipAddress = $_SERVER['REMOTE_ADDR'];
+
+// Capturar a data e hora atuais
+$accessTime = date('Y-m-d H:i:s');
+
+// Montar a linha de log
+$logEntry = "IP: $ipAddress - Access Time: $accessTime\n";
+
+// Abrir o arquivo de log para escrita
+$fileHandle = fopen($logFile, 'a');
+
+// Escrever a linha de log no arquivo
+fwrite($fileHandle, $logEntry);
+
+// Fechar o arquivo de log
+fclose($fileHandle);
+
+// Continuação do seu código para a landing page
+?>
   <body>
     <div class="corpo">
 
